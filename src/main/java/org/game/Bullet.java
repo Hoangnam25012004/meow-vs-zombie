736d8299cfc1e0e13f25ca1e7e233d2ab6451c83
My Thảo <mythao@Muys-MacBook.local>
2023-12-12 19:41:06 +0700

    em gui phan bullet

--- src/main/java/org/game/Bullet.java
+++ src/main/java/org/game/Bullet.java
@@ -1,4 +1,36 @@
 package org.game;
 
 public class Bullet {
+    private int x, y;
+    private int Dame;
+    private int ID;
+
+    public int getID() { return ID; }
+    public int getDame() { return Dame; }
+    public Bullet(int x, int y, int ATK, int ID){
+        this.x = x;
+        this.y = y;
+        this.Dame = Dame;
+        this.ID = ID;
+        }
+        
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY () {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public void move () {
+        if (this.x<1024) { 
+            this.x += 8;
+        }
+    }
 }
