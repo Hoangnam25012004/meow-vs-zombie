a2c88cdfd6b54d4e32b019b481092658e52495d4
Quân Red <miqua2004@gmail.com>
2023-12-16 10:02:47 +0700

    add graphic

--- src/main/java/org/game/graphic/Graphical.java
+++ src/main/java/org/game/graphic/Graphical.java
@@ -1,4 +1,11 @@
 package org.game.graphic;
 
-public class Graphical {
+import javax.swing.*;
+
+public class Graphical extends JPanel {
+
+    // screen setting
+    final int originalTileSize = 32; //32x32 tile
+    final int scale = 3; // set scale all to 3
+
 }
