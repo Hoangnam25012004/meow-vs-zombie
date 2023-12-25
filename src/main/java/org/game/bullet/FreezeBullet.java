bf965082231c982357b1b0c0da01e7177c3802df
mythao <dvthaomy44@gmail.com>
2023-12-25 18:56:59 +0700

    update Manager

--- src/main/java/org/game/bullet/FreezeBullet.java
+++ src/main/java/org/game/bullet/FreezeBullet.java
@@ -1,5 +1,5 @@
 package org.game.bullet;
-public class FreezeBullet extends BulletManager {
+public class FreezeBullet extends Bullet {
     public FreezeBullet(int x, int y, int Dame){
         super(x,y,Dame);
         super.setisFrozen(true);
